e7d3e40d8f985d461e119aa30114f2171599a7f4
m10rten <qmaartenenzo@gmail.com>
2023-05-07 22:22:01 +0200

    Initial create

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0
+
+- Initial create `react-tailwind-loaders` project. 🎉
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{tsx,jsx,html,css,scss}"],
+  theme: {},
+  plugins: [],
+};
