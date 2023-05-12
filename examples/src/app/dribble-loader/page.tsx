9a49138d08034190f7e7b8671300e2499574abf4
m10rten <qmaartenenzo@gmail.com>
2023-05-12 18:29:38 +0200

    examples updated

--- examples/src/app/dribble-loader/page.tsx
+++ examples/src/app/dribble-loader/page.tsx
@@ -17,7 +17,7 @@ export default function DribbleLoaderPage() {
         delay={1200} // this time will pass before the loader fades out, it starts counting after the component is mounted.
         forceUpdate={state} // not recommended to use this prop, but it's here if you need it
         count={6} // 1 - 10
-        speed={1.2} // 0.1 - 10
+        speed={1.2} // 0.5 - 5
         className="!duration-700 !gap-0" // add a `!` to the beginning of the className to override the default timings.
         childClassName="!w-16 mx-0" // give the dots a more red, rounded look and space them out
       />
--- examples/src/app/spinner-loader/page.tsx
+++ examples/src/app/spinner-loader/page.tsx
@@ -8,10 +8,10 @@ export default function SpinnerLoaderPage() {
         type="button"
         className="py-2.5 px-3 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
         <InlineSpinner
-          thumbColor="text-indigo-600"
-          trackColor="text-gray-400"
-          speed={1.2}
-          size="medium"
+          thumbColor="text-indigo-600" // color of the background of the spiner.
+          trackColor="text-gray-400" // color of the spinner track
+          speed={1.2} // 0.5 - 5
+          size="medium" // size of the spinner.
         />
         Loading...
       </button>
